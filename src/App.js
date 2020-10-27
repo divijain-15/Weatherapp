236276bb4cf1f6b6935a15f8c4d6caa39c67f1bd
divijain-15 <73190405+divijain-15@users.noreply.github.com>
2020-10-27 19:40:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 const api = {
-  key:"03fa91cc5c46da0dd5999dfccb8d9363", 
+  key:"API_KEY", 
   base:"https://api.openweathermap.org/data/2.5/"
 }
 
